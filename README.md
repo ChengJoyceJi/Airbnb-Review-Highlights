b5d466d6b83ad5875eee483dfce818ede72a301b
ChengJoyceJi <jicheng8524@hotmail.com>
2019-12-15 15:46:58 -0500

    keyphrase extraction

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Airbnb-Review-Highlights
 
-# Data source: http://insideairbnb.com/get-the-data.html
+## Installation
+Please use python 3.7
+
+To pip install pke from github:
+```
+pip install git+https://github.com/boudinfl/pke.git
+```
+pke also requires external resources that can be obtained using:
+```
+python -m nltk.downloader stopwords
+python -m nltk.downloader universal_tagset
+python -m spacy download en # download the english model
+```
+Then you can run the code to get key phrases from the review text:
+```
+python main.py <listing_id>
+```
+For example
+```
+> python main.py 38241551
+great locat
+place
+nice furnish
+```
+
+
+## References
+Data source: http://insideairbnb.com/get-the-data.html
+Keyphrase extraction: https://github.com/boudinfl/pke
--- main.py
+++ main.py
@@ -1,9 +1,49 @@
 from data import Data
+from pke.unsupervised import TopicRank
+import sys
+import os
 
 def main():
     data = Data()
     data.parse_input_data()
-    print(len(data.list_id_to_reviews))
+
+    list_id = sys.argv[1]
+
+    with open('temp_review_text.txt', 'w') as txt_file:
+        for review in data.list_id_to_reviews.get(list_id):
+            txt_file.write(review)
+
+    # create a TopicRank extractor
+    extractor = TopicRank()
+
+    # get all review text for the target listing
+    extractor.load_document(
+        input='temp_review_text.txt',
+        language="en"
+    )
+
+    # select the keyphrase candidates, for TopicRank the longest sequences of 
+    # nouns and adjectives
+    extractor.candidate_selection(pos={'NOUN', 'PROPN', 'ADJ'})
+
+    # weight the candidates using a random walk. The threshold parameter sets the
+    # minimum similarity for clustering, and the method parameter defines the 
+    # linkage method
+    extractor.candidate_weighting(
+        threshold=0.74,
+        method='average',
+        heuristic='frequent'
+    )
+
+    # print the n-highest (10) scored candidates
+    for (keyphrase, _) in extractor.get_n_best(n=3, stemming=True):
+        print(keyphrase)
+
+    os.remove("temp_review_text.txt")
+
+    
+    
+    
     
 if __name__ == '__main__':
     main()
